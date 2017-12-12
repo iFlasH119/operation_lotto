1a08c7ebcc546e1c8ddaf5598a149cb21143d244
timapheus <stark.tim@gmx.de>
2017-12-12 18:00:34 +0100

    Added Simulation for console and generated Documentation

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,3 +1,14 @@
+/*
+*
+*@author: Tim
+*compile with c++11
+*
+*
+*
+*/
+
+
+
 #include "functions.h"
 #include <iostream>
 #include <vector>
@@ -7,44 +18,90 @@ using namespace std;
 
 
 
-
+/**
+ * @brief 
+ * Unnecessary function that checks if the Array containts exactly 6 elements.
+ * 
+ * @param numbers 
+ * @return true 
+ * @return false 
+ */
 bool check_length(vector<int> numbers){
     if (numbers.size() ==  6){
         return true;
     }
     else return false;
 }
-
+/**
+ * @brief 
+ * Simple print function for console
+ * @param numbers 
+ */
 void print_numbers(vector<int> numbers){
     for (int i = 0; i < numbers.size(); ++i){
         cout << numbers[i] << ' ';
     }
     cout << '\n';
 }
-
+/**
+ * @brief 
+ * Generates random numbers and fills them into given array.
+ * @param numbers 
+ */
 void fill_random_numbers(vector<int> &numbers){
-    if (check_length(numbers)){
-        std::random_device rdev;
-        std::mt19937 rgen(rdev());
-        std::uniform_int_distribution<int> idist(1,49); 
+    std::random_device rdev;
+    std::mt19937 rgen(rdev());
+    std::uniform_int_distribution<int> idist(1,49); 
 
-        for (int i = 0; i < numbers.size(); i++){
-            do {
-                numbers[i] = idist(rgen);
-            } while(find(numbers.begin(), numbers.begin() +i, numbers[i]) != (numbers.begin()+i));
-        }
+    for (int i = 0; i < numbers.size(); i++){
+        do {
+            numbers[i] = idist(rgen);
+        } while(find(numbers.begin(), numbers.begin() +i, numbers[i]) != (numbers.begin()+i));
     }
-    else cout << "Das übergebene Array hat nicht die richtige Länge \n";
+}
+
+/**
+ * @brief 
+ * Little Simulation
+ * Compares two vectors (tip & numbers) and only ends if all numbers equal
+ * @param tip 
+ * @param numbers 
+ */
+void draw_numbers (vector<int> tip, vector<int> &numbers){
+    int count = 0;
+    int jackpot = 0;
+    while(jackpot < tip.size()){
+        fill_random_numbers(numbers);
+        count++;
+        for (int i = 0; i < numbers.size(); i++){
+            if (find(numbers.begin(), numbers.end(), tip[i]) != (numbers.end())){
+                jackpot++;
+            }
+            else {
+                jackpot = 0;
+                break;
+            }
+        } 
+    }
+    print_numbers(numbers);
+    print_numbers(tip);
+    cout << count << '\n';
 }
 
 
 
+
+
 int main(){
+    /**
+     * @brief 
+     * little Test
+     * 
+     */
 
     std::vector<int> test {1,2,3,4,5,6};
-    print_numbers(test);
-    fill_random_numbers(test);
-    print_numbers(test);
+    std::vector<int> drawn_numbers {0,0,0,0,0,0};
+    draw_numbers(test,drawn_numbers);
 
 
     return 0;
